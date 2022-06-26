ae85fcf91549737a7cad70f9a397552ad0a03d44
Gian Paris C. Agsam <104345695+agsamgian@users.noreply.github.com>
2022-06-26 22:30:59 +0800

    Create main.ps1

--- main.ps1
+++ main.ps1
@@ -2,6 +2,7 @@ $currentloc = Get-Location;
 $custompath = 'Temp\ps.dat';
 $fullpath = "{0}\{1}" -f $currentloc,$custompath;
 
+
 function Write-Color() {
      Param (
          [string] $text = $(Write-Error "You must specify some text"),
@@ -26,19 +27,28 @@ function Write-Color() {
      }
      $host.UI.RawUI.ForegroundColor = $startColor;
  }
+
+ function Get-Banner(){
+    [cmdletbinding()]
+    Param (
+    [string]$Ascii
+    )
+    Process {
+        (Write-Color "{cyan}",$Ascii);
+    }
+}
 function Get-FileExistence(){
 (Write-Color "{yellow}Checking If Output File Exists...");
 Start-Sleep -s 1.5;
 if(-not(Test-Path $fullpath)){
-     (Write-Color "{green}Creating Output File, Since it doesn't exist...");
+     (Write-Color "{white}[{green}+{white}]Creating Output File, Since it doesn't exist...");
     $customDirPath = '{0}\{1}' -f $currentloc,'Temp';
     $createTempDir = New-Item $customDirPath -ItemType Directory;
   if(Test-Path $customDirPath){
 New-Item $fullpath -ItemType File;
   }
      }elseif (Test-Path $fullpath) {
-     (Write-Color "{green}Output File Exists in ");
-     Write-Host $fullpath;
+     (Write-Color "{white}[{green}+{white}]Output File Exists in ", $fullpath);
      Start-Sleep -s 1.5;
      }
 }
@@ -48,15 +58,33 @@ function Get-LiveProcesses(){
      [string]$Ps,[string]$Path
      )
      Process {
+$osLabel = (Get-WmiObject Win32_OperatingSystem).Caption;
+$compname =  $(whoami.exe);
+$architecture = (Get-WmiObject Win32_OperatingSystem).OSArchitecture;
+$version = (Get-WmiObject Win32_OperatingSystem).Version;
+$buildnum = (Get-WmiObject Win32_OperatingSystem).BuildNumber;
 while($true){
-$GetProcesses = Get-Process | Select-Object -Property $ps;
+$GetProcesses = Get-Process | Select-Object -Property $Ps;
 $SetContent = Set-Content $Path $GetProcesses;
 $PsLength = (Get-Content $fullpath).Length;
+
 Start-Sleep -s 1.5
 Clear-Host;
-(Write-Color "{white}[{green}+{white}]Made By Gian Paris C. Agsam | Github: {green}https://github.com/agsamgian");
-Write-Host("Total Live Processes:", $PsLength)
-
+Get-Banner -Ascii @"
+██████╗ ███████╗██╗      ██╗     ██╗██╗   ██╗███████╗██████╗ ███████╗
+██╔══██╗██╔════╝██║      ██║     ██║██║   ██║██╔════╝██╔══██╗██╔════╝
+██████╔╝███████╗██║█████╗██║     ██║██║   ██║█████╗  ██████╔╝███████╗
+██╔═══╝ ╚════██║██║╚════╝██║     ██║╚██╗ ██╔╝██╔══╝  ██╔═══╝ ╚════██║
+██║     ███████║███████╗ ███████╗██║ ╚████╔╝ ███████╗██║     ███████║
+╚═╝     ╚══════╝╚══════╝ ╚══════╝╚═╝  ╚═══╝  ╚══════╝╚═╝     ╚═════
+"@
+(Write-Color "{white}[{green}+{white}]Made By FonderElite | Github: {green}https://github.com/FonderElite");
+(Write-Color "{green}System-User: {white}", $compname);
+(Write-Color "{green}Os: {white}", $osLabel);
+(Write-Color "{green}Os-Architecture: {white}", $architecture);
+(Write-Color "{green}Version: {white}", $version);
+(Write-Color "{green}Build-Number: {white}", $buildnum);
+(Write-Color "{green}Total Live Processes: {white}", $PsLength);
 
 }
      }
